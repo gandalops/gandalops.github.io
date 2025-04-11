165ec197401406cc5b322f9bfc32eea81bd02e89
devyogi7579 <yogidevelopment37@gmail.com>
2025-04-11 23:46:57 +0200

    Add permissions fix for GitHub Pages deploy + ignore _site/

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Terraform Journey: From Zero to Launching EC2 on AWS",
-        "excerpt":"🧠 Why I Chose Terraform Coming from a project management and academic background, I wanted to actually build things — infrastructure included. I chose Terraform because: It uses declarative syntax (easy to reason about) It’s widely used in DevSecOps teams It integrates well with AWS, GitHub Actions, Jenkins, and more...","categories": ["Terraform","DevSecOps"],
-        "tags": ["terraform","aws","ec2","iac","devops"],
-        "url": "/terraform/devsecops/2025/04/11/terraform-basics.html",
-        "teaser": null
-      }]
